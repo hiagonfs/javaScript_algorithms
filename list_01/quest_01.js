f378d10684402e5825b4b2ef27954f98f6088d46
David Souza <sdavidmedeiros@gmail.com>
2020-10-02 22:42:06 -0300

    Update quest_01.js

--- list_01/quest_01.js
+++ list_01/quest_01.js
@@ -3,34 +3,28 @@
 const io = require('console-read-write');
 
 async function main() {
+  const read = await io.read();
 
-  let read = await io.read();
-
-  let matrix = Array(1000).fill(0); 
+  const matrix = Array(1000).fill(0); 
 
   for(var i = 1; i < read; i++) {
     for(var j = 0; j < read; j++) {
       matrix[j] = i;
-    } 
-  }
+    }; 
+  };
 
   let numbers = []; 
 
   for(var i = 0; i < read; i++) {
     for(var j = 0; j < read; j++) {
-        if((matrix[i] * matrix[j]) > read && (matrix[i] % matrix[j] == 0) && (matrix[i] % matrix[j] < read)){
+        if((matrix[i] * matrix[j]) > read && (matrix[i] % matrix[j] == 0) && (matrix[i] % matrix[j] < read)) {
             numbers.push(matrix[i], matrix[j]);
             break;  
-        }
-    }
-  }
+        };
+    };
+  };
 
-  if(numbers.length == 2) {
-    console.log(numbers[0], numbers[1]); 
-  } else {
-    console.log('-1'); 
-  }
-
+  numbers.length === 2 ? console.log(numbers[0], numbers[1]) : console.log('-1');
 }
 
 main();
